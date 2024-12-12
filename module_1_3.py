357d354065cb02b7c15de9f1e702562600114100
OksanaKrasichko <totoroksi0@gmail.com>
2024-12-12 18:15:23 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Oksana" # переменная с значением str
+print('name:',name)
+age = 43     # переменная с значением int
+print('age:',age)
+new_age = 44    # переменная с значением int
+print('new_age:',new_age)
+is_student = True     # переменная с логическим значением True
+print("is student:",is_student)
