7c8d1db7ce3f8ad68318d92840203d3185738d83
OksanaKrasichko <totoroksi0@gmail.com>
2025-01-11 21:35:33 +0300

    Add files via upload

--- переменные.py
+++ переменные.py
@@ -1,5 +1,5 @@
 homework_complete = 12
 hours = 1.5
 name = 'Python'
-hours_home = 1.5/12
+hours_home = hours/homework_complete
 print('курс:',name,',всего задач:',homework_complete,',затрачено часов:',hours,',среднее время выполнения:',hours_home,'.')
